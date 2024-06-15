be957a2312741695672ab83b804316e76ef29451
Cesar Aguilar <Blueavian9@gmail.com>
2024-06-15 14:09:13 -0700

    initial express api

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.post('');
+
 app.listen(port, () =>
   console.log(`server running on http://localhost:${port}`)
 );
